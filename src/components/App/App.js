5628ffaf801e85ba703e549cb7b221974c6a1563
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-04-22 14:20:39 -0500

    App.js: commented out unnecessary routes, imported AddPlant component at /addPlant, AddPlant.js: created form, created handleChangeFor, created local state for newPlant,  started handleSubmit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,7 @@ import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
+import AddPlant from '../AddPlant/AddPlant'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
@@ -20,11 +21,13 @@ import InfoPage from '../InfoPage/InfoPage';
 import './App.css';
 
 class App extends Component {
+  
   componentDidMount () {
     this.props.dispatch({type: 'FETCH_USER'})
   }
 
   render() {
+
     return (
       <Router>
         <div>
@@ -36,27 +39,27 @@ class App extends Component {
             This is a route anyone can see, no login necessary */}
             <Route
               exact
-              path="/about"
-              component={AboutPage}
+              path="/addPlant"
+              component={AddPlant}
             />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
+            {/* <ProtectedRoute
               exact
               path="/home"
               component={UserPage}
-            />
+            /> */}
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <ProtectedRoute
+            {/* <ProtectedRoute
               exact
               path="/info"
               component={InfoPage}
             />
             {/* If none of the other routes matched, we will show a 404. */}
-            <Route render={() => <h1>404</h1>} />
+            {/* <Route render={() => <h1>404</h1>} /> */} */}
           </Switch>
           <Footer />
         </div>
