3cbacc27ce254425bb93fa7f9860e7b28291973c
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-04-22 12:13:33 -0500

    database.sql: added additional table data

--- database.sql
+++ database.sql
@@ -8,3 +8,30 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
+CREATE TABLE "plants" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(80) NOT NULL,
+	"image" varchar(600),
+	"days_to_water" int NOT NULL,
+	"date_added" DATE NOT NULL,
+	"status" boolean DEFAULT true,
+	"room_id" INT REFERENCES "rooms",
+	"sun_id" INT REFERENCES "sunlight"
+);
+
+CREATE TABLE "rooms" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(80)
+);
+
+CREATE TABLE "sunlight" (
+	"id" SERIAL PRIMARY KEY,
+	"light" text
+);
+
+INSERT INTO "rooms" ("name")
+VALUES ('Kitchen'), ('Dining Room'), ('Living Room'), ('Family Room'), ('Bathroom'), ('Bedroom'), ('Porch');
+
+INSERT INTO "sunlight" ("light") VALUES ('Low Light'), ('Moderate Light'), ('Bright Light');
