c8aa36870762d51d13b8b7e29215fad96c5f4697
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-04-26 16:19:19 -0500

    created material sort-by select dropdown

--- src/components/PlantInventory/PlantInventory.js
+++ src/components/PlantInventory/PlantInventory.js
@@ -6,6 +6,8 @@ import PlantItem from '../PlantItem/PlantItem';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = theme => ({
     root: {
@@ -16,9 +18,44 @@ const styles = theme => ({
         textAlign: 'center',
         color: theme.palette.text.secondary,
     },
+    textField: {
+        marginLeft: 124,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    cssLabel: {
+        '&$cssFocused': {
+            color: '#9db1b1;',
+        },
+    },
+    cssFocused: {},
+    cssUnderline: {
+        '&:after': {
+            borderBottomColor: '#9db1b1;',
+        },
+    },
+    cssOutlinedInput: {
+        '&$cssFocused $notchedOutline': {
+            borderColor: '#9db1b1;',
+        },
+    },
 });
 
+const sortList = [{ name: 'Room' }, { name: 'Light Requirement' }, { name: 'Water Status' }, { name: 'Days to Water' }, { name: 'Days in Family' }];
+
 class PlantInventory extends Component {
+
+    state = {
+        sort_by: '',
+    }
+
+    handleChangeFor =  value => event => {
+        event.preventDefault();
+        this.setState({
+            sort_by: value
+        })
+        this.props.dispatch({ type: 'SORT_BY', payload: this.state })
+    }
 
     componentDidMount = () => {
         this.props.dispatch({ type: 'GET_PLANTS' })
@@ -31,6 +68,40 @@ class PlantInventory extends Component {
                 <h4 id='plantHeader'>PLANTS</h4>
                 {this.props.reduxState.plantListReducer === '' ? null : this.props.reduxState.plantListReducer.length === 0 ? <div> <h1>You need to add some plants to your family!</h1> <button className="add-plants-btn">Add Plants</button></div> :
                     <div>
+                        <div>
+                            <TextField
+                                id="standard-select"
+                                select
+                                label="Sort By"
+                                className={classes.textField}
+                                value={this.state.sort_by}
+                                onChange={this.handleChangeFor('sort_by')}
+                                SelectProps={{
+                                    MenuProps: {
+                                        className: classes.menu,
+                                    },
+                                }}
+                                InputLabelProps={{
+                                    classes: {
+                                        root: classes.cssLabel,
+                                        focused: classes.cssFocused,
+                                    },
+                                }}
+                                InputProps={{
+                                    classes: {
+                                        root: classes.cssOutlinedInput,
+                                        focused: classes.cssFocused,
+                                        underline: classes.cssUnderline,
+                                    },
+                                }}
+                                helperText="Please select your plant's sunlight requirements"
+                                margin="normal"
+                            >
+                                {sortList.map((item, i) =>
+                                    <MenuItem value={i} key={i}>{item.name}</MenuItem>
+                                )}
+                            </TextField>
+                        </div>
                         <p id='plantCount'>plants in the fam: {this.props.reduxState.plantListReducer.length}</p>
                         <Grid container spacing={24} className={classes.mainGrid} >
                             {/* maps through projects reducer and displays each project on dom*/}
