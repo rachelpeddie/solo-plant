abce8548e87f78bc5d1e7a4f21ebf0f7cf67f7eb
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-05-04 14:16:17 -0500

    successfully updating plant statuses with serverside logic, still getting error message in cosole

--- server/routes/water.router.js
+++ server/routes/water.router.js
@@ -64,12 +64,72 @@ function checkWater (){
             })
     }
 
+waterStatus = (plant) => {
+    console.log(`this will change water status for`, plant.nickname);
+    axios.put(`/:${plant.id}`) 
+        // let plant = req.body;
+        let sqlText = `UPDATE "plants" SET "status" = $1 WHERE "plants"."id" = $2;`;
+        // console.log(`req.params are`, req.params);
+        // console.log(`req.body is`, req.body);
+        pool.query(sqlText, [!plant.status, plant.id])
+            .then(response => {
+                console.log(`Woot!  Successfully updated plant status`);
+                // res.sendStatus(201);
+            }).catch(error => {
+                console.log(`error updating plant status in database`, error);
+                // res.sendStatus(500);
+            })
+}
+
+needsWaterCalc = (plant) => {
+    // console.log(`water is`, plant);
+
+    const now = moment().format();
+    const expiration = moment(plant.last_watered);
+
+    // get the difference between the moments
+    const diff = expiration.diff(now);
+
+    //express as a duration
+    const diffDuration = moment.duration(diff);
+    console.log(diffDuration.days());
+
+
+    let days = (diffDuration.days() + plant.days_to_water)
+
+    // console.log(`${plant.nickname}'s days are`, days);
+
+    if (days <= 0 && plant.status === true) {
+        console.log(`${plant.nickname} needs some water!`);
+        waterStatus(plant);
+    }
+    return Math.abs(days);
+}
+
+function changeStatus() {
+    axios.get('/')
+    let sqlText = `SELECT * FROM "plants";`;
+    pool.query(sqlText)
+        .then(response => {
+            // console.log(`in scheduled cron check`, response.rows);
+            for( plant of response.rows ) {
+            needsWaterCalc(plant);
+            }
+        })
+        .catch(error => {
+            console.log(`error checking plant status`, error);
+        })
+}
+
 
 cron.schedule('* * * * *', () => {
     console.log(`running node cron every minute`);
+    changeStatus();
     // checkWater();
 },
     {
         scheduled: true,
     });
+
+
 module.exports = router;
